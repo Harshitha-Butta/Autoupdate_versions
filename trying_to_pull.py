4578286345e0a0cc47f581f348d58021a8365f82
Harshitha-Butta <harshitha.butta@gmail.com>
2023-01-04 23:28:51 +0530

    git module added

--- trying_to_pull.py
+++ trying_to_pull.py
@@ -1,6 +1,6 @@
 from git import Repo
 
-repo=Repo('https://github.com/Harshitha-Butta/Autoupdate_versions')
+repo=Repo('https:\\github.com\Harshitha-Butta\Autoupdate_versions\blob\main\versions_hyd_host2.json')
 
 p=repo.remotes.origin
 
