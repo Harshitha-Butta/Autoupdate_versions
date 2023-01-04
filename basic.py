fed327fb7b3323dfc7c65b182aaef16aee5eadf2
Harshitha-Butta <harshitha.butta@gmail.com>
2023-01-05 00:19:20 +0530

    commit message from python script

--- basic.py
+++ basic.py
@@ -32,5 +32,5 @@ add_version(data)
 
 g.add('--all')
 g.commit('-m', 'commit message from python script', author='harshitha.butta@gmail.com')
-origin = repo.git.remote("origin")
+origin = git.repo.remote("origin")
 origin.push()
