008d66fc1b756391423be9e818549e02505a8f2c
Butta-Harshitha <hb185098@ncr.com>
2023-01-06 16:30:22 +0530

    all changes

--- basic.py
+++ basic.py
@@ -35,8 +35,8 @@ add_version(data)
 
 repo=Repo(r'C:\git practice\checkout_branch\Autoupdate_versions')
 
-g.add('--all')
-g.commit('-m', 'commit message from python script', author='harshitha.butta@gmail.com')
+repo.add('--all')
+repo.commit('-m', 'commit message from python script', author='harshitha.butta@gmail.com')
 origin = repo.remote(name="origin")
 origin.push()
 g.checkout('main')
